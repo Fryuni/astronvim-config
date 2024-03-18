71963d0793cc0c2ae9c06dc02c568c57ca4df031
Luiz Ferraz <luiz@lferraz.com>
2024-03-18 18:55:28 -0300

    Add language packs

--- lua/community.lua
+++ lua/community.lua
@@ -6,7 +6,17 @@
 return {
   -- TODO: Remove branch v4 on release
   { "AstroNvim/astrocommunity", branch = "v4" },
+
+  -- Language packs
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.astro" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.zig" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.yaml" },
+
   { import = "astrocommunity.completion.copilot-lua" },
-  -- import/override with your plugins folder
 }
