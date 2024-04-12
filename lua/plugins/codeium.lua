ccbab623bad4dda51a0efaea7cfcfcbcbcbd7e67
Luiz Ferraz <luiz@lferraz.com>
2024-04-12 00:12:22 -0300

    Simplify Codeium config

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -7,54 +7,58 @@ return {
   dependencies = {
     {
       "AstroNvim/astrocore",
-      opts = function(_, opts)
-        local maps = opts.mappings
+      ---@type AstroCoreOpts
+      opts = {
+        mappings = {
+          i = {
+            ["<c-i>"] = {
+              function() return vim.fn["codeium#Accept"]() end,
+              expr = true,
+              silent = true,
+              desc = "Codeium Accept",
+            },
+            ["<c-.>"] = {
+              function() return vim.fn["codeium#CycleOrComplete"]() end,
+              expr = true,
+              silent = true,
+              desc = "Codeium Cycle",
+            },
+            ["<c-,>"] = {
+              function() return vim.fn["codeium#CycleCompletions"](-1) end,
+              expr = true,
+              silent = true,
+              desc = "Codeium Previous",
+            },
+            ["<c-x>"] = {
+              function() return vim.fn["codeium#Clear"]() end,
+              expr = true,
+              silent = true,
+              desc = "Codeium Clear",
+            },
+          },
+          n = {
+            ["<Leader>;"] = { desc = "Codeium" },
+            ["<Leader>;c"] = {
+              function() return vim.fn["codeium#Chat"]() end,
+              desc = "Codeium Chat",
+            },
+            ["<Leader>;;"] = {
+              desc = "Toggle Codeium",
+              function()
+                local notify = require("astrocore").notify
 
-        maps.i["<c-i>"] = {
-          function() return vim.fn["codeium#Accept"]() end,
-          expr = true,
-          silent = true,
-          desc = "Codeium Accept",
-        }
-        maps.i["<c-.>"] = {
-          function() return vim.fn["codeium#CycleOrComplete"]() end,
-          expr = true,
-          silent = true,
-          desc = "Codeium Cycle",
-        }
-        maps.i["<c-,>"] = {
-          function() return vim.fn["codeium#CycleCompletions"](-1) end,
-          expr = true,
-          silent = true,
-          desc = "Codeium Previous",
-        }
-        maps.i["<c-x>"] = {
-          function() return vim.fn["codeium#Clear"]() end,
-          expr = true,
-          silent = true,
-          desc = "Codeium Clear",
-        }
-
-        maps.n["<Leader>;"] = { desc = "Codeium" }
-        maps.n["<Leader>;c"] = {
-          function() return vim.fn["codeium#Chat"]() end,
-          desc = "Codeium Chat",
-        }
-        maps.n["<Leader>;;"] = {
-          desc = "Toggle Codeium",
-          function()
-            local notify = require("astrocore").notify
-
-            if vim.g.codeium_enabled == true then
-              vim.cmd "CodeiumDisable"
-              notify("Disabled Codeium", vim.log.levels.WARN)
-            else
-              vim.cmd "CodeiumEnable"
-              notify("Disabled Codeium", vim.log.levels.WARN)
-            end
-          end,
-        }
-      end,
+                if vim.g.codeium_enabled == true then
+                  vim.cmd "CodeiumDisable"
+                  notify("Disabled Codeium", vim.log.levels.WARN)
+                else
+                  vim.cmd "CodeiumEnable"
+                  notify("Disabled Codeium", vim.log.levels.WARN)
+                end
+              end,
+            },
+          },
+        },
+      },
     },
     {
       "rebelot/heirline.nvim",
