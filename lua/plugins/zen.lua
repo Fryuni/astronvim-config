ebb529f72b71e0171ee5d411bf0a55c925b021a9
Luiz Ferraz <luiz@lferraz.com>
2025-08-02 16:16:27 -0300

    Configure Zen mode with Twilight

--- lua/plugins/zen.lua
+++ lua/plugins/zen.lua
@@ -3,33 +3,61 @@ return {
   {
     "folke/zen-mode.nvim",
     cmd = { "ZenMode" },
+    keys = {
+      {
+        "<Leader>zz",
+        "<cmd>ZenMode<CR>",
+        desc = "Zen Mode",
+      },
+    },
     -- https://github.com/folke/zen-mode.nvim#%EF%B8%8F-configuration
-    ---@type ZenOptions
+    ---@type ZenMode
     opts = {
-      options = {
-        signcolumn = "no", -- Disable sign column
-        foldcolumn = "0", -- Disable fold column
-        cursorline = false, -- Disable cursor line
-        colorcolumn = "0", -- Disable color column
-        showcmd = false, -- Disable command line
-        ruler = false, -- Disable ruler
-        laststatus = 0, -- Disable status line
+      window = {
+        width = 0.5, -- Width of the zen window
+        height = 0.8, -- Height of the zen window
+        options = {
+          signcolumn = "no", -- Disable sign column
+          foldcolumn = "0", -- Disable fold column
+          cursorline = false, -- Disable cursor line
+          colorcolumn = "0", -- Disable color column
+          laststatus = 0, -- Disable status line
+        },
+      },
+      plugins = {
+        options = {
+          showcmd = false, -- Disable command line
+          ruler = false, -- Disable ruler
+        },
       },
       wezterm = {
         enabled = true, -- Enable/disable wezterm integration
         font_size = "+6", -- Font size to use in wezterm
       },
     },
-  },
-  {
-    "AstroNvim/astrocore",
-    ---@type AstroCoreOpts
-    opts = {
-      mappings = {
-        n = {
-          ["<Leader>zz"] = { "<cmd>ZenMode<CR>", desc = "Zen Mode" },
+    dependencies = {
+      {
+        "folke/twilight.nvim",
+        opts = {
+          expand = {
+            "function",
+            "method",
+            "table",
+            "if_statement",
+          },
         },
       },
     },
   },
+  -- {
+  --   "AstroNvim/astrocore",
+  --   ---@type AstroCoreOpts
+  --   opts = {
+  --     mappings = {
+  --       n = {
+  --         ["<Leader>zz"] = { "<cmd>ZenMode<CR>", desc = "Zen Mode" },
+  --       },
+  --     },
+  --   },
+  -- },
 }
