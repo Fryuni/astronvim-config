7d6b94a2a17bf0898e5ff1bc546ef633ba8a8000
Luiz Ferraz <luiz@lferraz.com>
2025-08-13 11:50:33 -0300

    Update Python config to work equally on NixOS and Mac

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
--- Set up Python support before any plugins are loaded
-vim.g.python3_host_prog = vim.fn.expand "~/.config/nvim/venv/bin/python"
+-- Update environment variables for python venv
+vim.env.PATH = vim.fn.expand "~/.config/nvim/venv/bin" .. ":" .. vim.env.PATH
+vim.env.VIRTUAL_ENV = vim.fn.expand "~/.config/nvim/venv"
 
 -- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
 -- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
