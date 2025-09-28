5db6d83082ddf59c504e550991a5d270a016acd3
Luiz Ferraz <luiz@lferraz.com>
2025-09-28 20:52:06 -0300

    Configure Zig debugging

--- lua/plugins/zig.lua
+++ lua/plugins/zig.lua
@@ -1,3 +1,4 @@
+---@type LazySpec
 return {
   {
     "nvim-treesitter/nvim-treesitter",
@@ -23,6 +24,44 @@ return {
     opts = function(plugin, opts)
       opts.servers = opts.servers or {}
       vim.list_extend(opts.servers, { "zls" })
+    end,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    ft = { "zig" },
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "codelldb" })
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    ft = { "zig" },
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "codelldb" })
+    end,
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      -- On zig projects, register a debug configuration that runs the test binary
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "zig",
+        callback = function()
+          local dap = require "dap"
+          dap.configurations.zig = {
+            {
+              name = "Debug test",
+              type = "codelldb",
+              request = "launch",
+              program = "${workspaceFolder}/zig-out/bin/test",
+              args = {},
+              cwd = "${workspaceFolder}",
+              stopOnEntry = false,
+            },
+          }
+        end,
+      })
     end,
   },
 }
