e8d3c7d22bc23fff22a77da1c8bcaca4279160dd
Luiz Ferraz <luiz@lferraz.com>
2025-12-09 11:27:18 -0300

    Disable unused packs

--- lua/community.lua
+++ lua/community.lua
@@ -13,10 +13,10 @@ return {
   { import = "astrocommunity.pack.go" },
   -- { import = "astrocommunity.pack.php" },
   { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.pack.nix" },
+  -- { import = "astrocommunity.pack.nix" },
   -- { import = "astrocommunity.pack.python" },
-  { import = "astrocommunity.pack.terraform" },
   { import = "astrocommunity.pack.typescript" },
+  -- { import = "astrocommunity.pack.terraform" },
   -- { import = "astrocommunity.pack.json" },
   -- { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.pack.markdown" },
@@ -24,17 +24,17 @@ return {
   { import = "astrocommunity.pack.sql" },
 
   { import = "astrocommunity.lsp.ts-error-translator-nvim" },
-  { import = "astrocommunity.quickfix.nvim-bqf" },
+  -- { import = "astrocommunity.quickfix.nvim-bqf" },
 
   { import = "astrocommunity.editing-support.cloak-nvim" },
   { import = "astrocommunity.motion.harpoon" },
   { import = "astrocommunity.media.vim-wakatime" },
   { import = "astrocommunity.completion.copilot-lua" },
   -- { import = "astrocommunity.editing-support.copilotchat-nvim" },
-  { import = "astrocommunity.editing-support.codecompanion-nvim" },
+  -- { import = "astrocommunity.editing-support.codecompanion-nvim" },
 
   -- { import = "astrocommunity.note-taking.obsidian-nvim" },
 
-  { import = "astrocommunity.recipes.astrolsp-no-insert-inlay-hints" },
+  -- { import = "astrocommunity.recipes.astrolsp-no-insert-inlay-hints" },
   { import = "astrocommunity.recipes.picker-lsp-mappings" },
 }
